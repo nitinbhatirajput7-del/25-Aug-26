a9e5c99aa1a33dcb23cba9fb710288887cab8292
Nitin Bhati <nitinbhatirajput7@gmail.com>
2026-08-25 22:50:15 +0530

    modified file

---
+++ testing.py
@@ -0,0 +1,40 @@
+  477  git status
+  478  git status
+  479  git commit -a
+  480  git status
+  481  git init
+  482  .git
+  483  rm -rf .git
+  484  git status
+  485  git status
+  486  mkdir user/nitin/Desktop
+  487  mkdir nitin
+  488  pwd
+  489  rmdir nitin
+  490  git init
+  491  mkdir foldermaster
+  492  cd foldermaster/
+  493  cat >fileone
+  494  git add fileone
+  495  git commit -m task1 fileone
+  496  git remote add origin https://github.com/nitinbhatirajput7-del/first-repo.git
+  497  git push origin master
+  498  git remote add origin https://github.com/nitinbhatirajput7-del/first-repo.git
+  499  git push origin master
+  500  pwd
+  501  touch testing.py
+  502  ls
+  503  ls -l
+  504  git init
+  505  ls
+  506  ls -l
+  507  ll
+  508  ls -la
+  509  git status
+  510  git add .
+  511  git status
+  512  git commit -m "firsr-file" testing.py
+  513  git log
+  514  git branch
+  515  git rm --cached testing.py
+  516  git s
